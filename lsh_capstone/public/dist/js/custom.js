c8771a1e10e38b1149abf56d06391d70738c8983
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-05-11 09:03:36 +0800

    fixed the code structure of the background image function

--- lsh_capstone/public/dist/js/custom.js
+++ lsh_capstone/public/dist/js/custom.js
@@ -18,10 +18,10 @@
         var header = $('body');
         
         var backgrounds = new Array(
-            'url(https://assets.hiphotels.com/images/media/518819/brown_s_hotel_er_17.jpg)'
-          , 'url(https://celebratedexperiences.com/app/uploads/2016/09/BRO-misc-34-1152x768.jpg)'
-          , 'url(https://celebratedexperiences.com/app/uploads/2016/09/BRO-room-Classic-Suite6-1536x1024.jpg)'
-          , 'url(https://cdn.kiwicollection.com/media/room_images/PR003135/xl/pr003135-c1s-deluxesuite.jpg)'
+            'url(https://assets.hiphotels.com/images/media/518819/brown_s_hotel_er_17.jpg)',
+             'url(https://celebratedexperiences.com/app/uploads/2016/09/BRO-misc-34-1152x768.jpg)',
+             'url(https://celebratedexperiences.com/app/uploads/2016/09/BRO-room-Classic-Suite6-1536x1024.jpg)',
+             'url(https://cdn.kiwicollection.com/media/room_images/PR003135/xl/pr003135-c1s-deluxesuite.jpg)'
         );
         
         var current = 0;
