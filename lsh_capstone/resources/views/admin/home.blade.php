4b21af51bd3e49302c480a6cb1a97b437e2abb9e
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-05-12 20:32:37 +0800

    removed the unnecessary cards in the admin dashboard

--- lsh_capstone/resources/views/admin/home.blade.php
+++ lsh_capstone/resources/views/admin/home.blade.php
@@ -106,57 +106,6 @@
             </div>
         </a>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-        <a href="{{ route('admin_subscriber_show') }}">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-website">
-                    <i class="fa fa-users"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Total Apartments</h4>
-                    </div>
-                    <div class="card-body">
-                        3
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-        <a href="{{ route('admin_subscriber_show') }}">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-website">
-                    <i class="fa fa-users"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Total Hotels</h4>
-                    </div>
-                    <div class="card-body">
-                        2
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-        <a href="{{ route('admin_subscriber_show') }}">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-website">
-                    <i class="fa fa-users"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Total Boarding Houses</h4>
-                    </div>
-                    <div class="card-body">
-                        2
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
 
 
 
