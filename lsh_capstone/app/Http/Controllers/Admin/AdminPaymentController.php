7ae898782ce6b3d4570aafa01d40d03aae93919f
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-05-13 16:02:23 +0800

    added an index function in the admin payment controller

---
+++ lsh_capstone/app/Http/Controllers/Admin/AdminPaymentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Payment;
+use Illuminate\Http\Request;
+
+class AdminPaymentController extends Controller
+{
+    public function index()
+    {
+        $payment_info = Payment::where('id', 1)->first();
+
+        return view('admin.payment', compact('payment_info'));
+    }
+
+    
+}
