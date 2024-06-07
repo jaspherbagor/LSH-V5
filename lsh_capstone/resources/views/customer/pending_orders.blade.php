dc3395ca2b0a7390291134ca172d31c524098b34
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-06-07 08:43:38 +0800

    removed the commented codes

--- lsh_capstone/resources/views/customer/pending_orders.blade.php
+++ lsh_capstone/resources/views/customer/pending_orders.blade.php
@@ -23,12 +23,6 @@
                                 </thead>
                                 <tbody>
                                     @foreach($pending_orders as $row)
-
-                                    {{-- @php
-                                    $order_detail = \App\Models\OrderDetail::where('order_no', $row->order_no)->first();
-                                    $room = \App\Models\Room::where('id', $order_detail->room_id)->first();
-                                    $accommodation = \App\Models\Accommodation::where('id', $room->accommodation_id)->first(); 
-                                    @endphp --}}
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
                                         <td>{{ $row->order_no }}</td>
